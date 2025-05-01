66c2003ac727709f15f68a286081728c7af49f8a
Arjunk595 <arjunkendre15@gmail.com>
2025-05-01 23:58:19 +0530

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -1,5 +1,5 @@
 import collections
-
+# BFS
 def bfs(graph,root):
     visited=set()
     queue=collections.deque([root])
@@ -11,10 +11,29 @@ def bfs(graph,root):
                 if i not in visited:
                     queue.append(i)
     print(visited)
-    
+# DFS
+visited1 = set()
+
+def DFS(visited1, graph, root):
+    if root not in visited1:
+        print(root,end=" ")
+        visited1.add(root)
+        for neighbour in graph[root]:
+            DFS(visited1, graph, neighbour)
+   
 if __name__=="__main__":
     graph={0:[1,2,3],1:[0,2],2:[0,1,4],3:[0],4:[2]}
+    
+    graph1={
+        0:[1,2,3],
+        1:[2],
+        2:[4],
+        3:[],
+        4:[],
+    }
+    
     bfs(graph,0)
+    DFS(visited1,graph1,0)
     
 #1. Time complexity for DFS & BFS
     # O(V + E) => V : number of vertices, E : number of edges
